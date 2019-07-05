4168fe9a8b76888cf6cec35da4846e8e872ed060
Yuhei Yamaya <s1260104@u-aizu.ac.jp>
2019-07-05 12:11:52 +0900

    Initial revision

---
+++ DiceGame.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+
+public class DiceGame {
+    public static void main(String[] ages){
+	int[] dice = {1,2,3,4,5,6};
+	int dis=0;
+	int total=0;
+	Random r = new Random();
+	
+	System.out.println("Rolling the dice...");
+	
+	for(int i = 0; i < 2 ; i ++){
+	    dis = dice[r.nextInt(5)];
+	    total += dis;
+	    System.out.println("Die " + (i+1) + ": " + dis);
+	}
+	System.out.println("Total value: " + total);
+    }
+}
