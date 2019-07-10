bfde5bb1eaae5534c4499381e59afc922f01abbb
Yuhei Yamaya <s1260104@u-aizu.ac.jp>
2019-07-10 16:15:00 +0900

    add original

--- DiceGame.java
+++ DiceGame.java
@@ -6,6 +6,7 @@ public class DiceGame {
 	int[] dice = {1,2,3,4,5,6};
 	int dis=0;
 	int total=0;
+	int count=0;
 	Random r = new Random();
 	
 	System.out.println("What is your name?");
@@ -17,10 +18,12 @@ public class DiceGame {
 	
 	for(int i = 0; i < 2 ; i ++){
 	    dis = dice[r.nextInt(5)];
+	    if(dis > 3) count++;
 	    total += dis;
 	    System.out.println("Die " + (i+1) + ": " + dis);
 	}
 	System.out.println("Total value: " + total);
+	System.out.println("Number of times greater than 3: " + count);
 	if(total > 7) System.out.println(str + " won!");
 	else System.out.println(str + " lost!");
     }
